17624e2ddddb7e11b6d20bb1cdbd3b38ff21fee6
Шумилин Александр Владимирович <a.v.shumilin@voentelecom.ru>
2017-10-11 17:22:19 +0300

    collections

--- src/com/geekbrains/Main.java
+++ src/com/geekbrains/Main.java
@@ -1,5 +1,6 @@
 package com.geekbrains;
 
+import com.geekbrains.Collections_lesson.PhoneBook;
 import com.geekbrains.Exception_lesson.ArrayChecker;
 import com.geekbrains.oop_lesson.obstacleCourse.*;
 import com.geekbrains.oop_lesson.team.Team;
@@ -8,10 +9,29 @@ import com.geekbrains.oop_lesson.team.TeamMember;
 public class Main {
 
     public static void main(String[] args) {
-        doLesson_2();
+        doLesson_3();
     }
 
-    public static void doLesson_1 (){
+    private static void doLesson_3() {
+        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.addElementPhoneBook("Shum","89670049109");
+        phoneBook.addElementPhoneBook("Shum","89670049110");
+        phoneBook.addElementPhoneBook("Shum","89670049111");
+
+        phoneBook.addElementPhoneBook("Art","888888");
+        phoneBook.addElementPhoneBook("Art","888888");
+        phoneBook.addElementPhoneBook("Art","888888   ");
+        phoneBook.addElementPhoneBook("Art","888888 ");
+        phoneBook.addElementPhoneBook("Art","888888     ");
+        phoneBook.addElementPhoneBook("Art","888888");
+        phoneBook.addElementPhoneBook("Art","888888");
+
+        System.out.println("Shum: " + phoneBook.getPhonesByName("Shum"));
+        System.out.println("Art: " + phoneBook.getPhonesByName("Art"));
+        System.out.println("Alex: " + phoneBook.getPhonesByName("Alex"));
+    }
+
+    private static void doLesson_1 (){
         Obstacle[] obstacle = {new Cross(6), new Swimming(3), new Fight(9),new Jumping(1)};
         Course c = new Course(obstacle); // Создаем полосу препятствий
 
@@ -26,7 +46,7 @@ public class Main {
         team.showResults(); // Показываем результаты первой команды
     }
 
-    public static void doLesson_2(){
+    private static void doLesson_2(){
 
         String[][] strings = createArray();
         //strings[2][3]="K";
@@ -36,7 +56,7 @@ public class Main {
         arrayChecker.summ();
     }
 
-    public static String[][] createArray() {
+    private static String[][] createArray() {
         String[][] array = new String[4][4];
         for (int i = 0; i < array.length; i++,System.out.println()) {
             for (int j = 0; j < array[i].length; j++) {
