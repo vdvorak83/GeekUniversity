eb23fa7e59fad6ea66ee0556d6d5877e7009ae72
Шумилин Александр Владимирович <a.v.shumilin@voentelecom.ru>
2017-11-09 17:11:39 +0300

    no message

--- src/com/geekbrains/Main.java
+++ src/com/geekbrains/Main.java
@@ -15,55 +15,7 @@ import com.geekbrains.MultiThreading.*;
 public class Main {
 
     public static void main(String[] args) {
-        doLesson_6();
-    }
 
-    private static void doLesson_6() {
-        // run two methods in class ClientLauncher and ServerLauncher
-    }
-
-    private static String[] createArrayWords() {
-        String [] strings = new String[10];
-        strings[0] = "Mama";
-        strings[1] = "Papa";
-        strings[2] = "Puma";
-        strings[3] = "Irbis";
-        strings[4] = "Mama";
-        strings[5] = "Papa";
-        strings[6] = "Kot";
-        strings[7] = "Sobaka";
-        strings[8] = "Dochka";
-        strings[9] = "Sobaka";
-        return strings;
-    }
-
-    private static void doPhoneBook(){
-        PhoneBook phoneBook = new PhoneBook();
-        phoneBook.addElementPhoneBook("Shum","89670049109");
-        phoneBook.addElementPhoneBook("Shum","89670049110");
-        phoneBook.addElementPhoneBook("Shum","89670049111");
-
-        phoneBook.addElementPhoneBook("Art","888888");
-        phoneBook.addElementPhoneBook("Art","888888");
-        phoneBook.addElementPhoneBook("Art","888888   ");
-        phoneBook.addElementPhoneBook("Art","888  88 8 ");
-        phoneBook.addElementPhoneBook("Art","8  8  8 888     ");
-
-        System.out.println("Shum: " + phoneBook.getPhonesByName("Shum"));
-        System.out.println("Art: " + phoneBook.getPhonesByName("Art"));
-        System.out.println("Alex: " + phoneBook.getPhonesByName("Alex"));
-        System.out.println("Nikto:" + phoneBook.getPhonesByName(""));
-    }
-
-    private static String[][] createArray() {
-        String[][] array = new String[4][4];
-        for (int i = 0; i < array.length; i++,System.out.println()) {
-            for (int j = 0; j < array[i].length; j++) {
-                array[i][j] = String.valueOf((int)(Math.random() * 10));
-                System.out.print(array[i][j] + " ");
-            }
-        }
-        return array;
     }
 
     private static void doLesson_1 (){
@@ -105,6 +57,54 @@ public class Main {
         MultiThreadingCalculate multiThreadingCalculate = new MultiThreadingCalculate();
         multiThreadingCalculate.oneThreadCalculate();
         multiThreadingCalculate.twoThreadsCalculate();
+    }
+
+    private static void doLesson_6() {
+        // run two methods in class ClientLauncher and ServerLauncher
+    }
+
+    private static void doPhoneBook(){
+        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.addElementPhoneBook("Shum","89670049109");
+        phoneBook.addElementPhoneBook("Shum","89670049110");
+        phoneBook.addElementPhoneBook("Shum","89670049111");
+
+        phoneBook.addElementPhoneBook("Art","888888");
+        phoneBook.addElementPhoneBook("Art","888888");
+        phoneBook.addElementPhoneBook("Art","888888   ");
+        phoneBook.addElementPhoneBook("Art","888  88 8 ");
+        phoneBook.addElementPhoneBook("Art","8  8  8 888     ");
+
+        System.out.println("Shum: " + phoneBook.getPhonesByName("Shum"));
+        System.out.println("Art: " + phoneBook.getPhonesByName("Art"));
+        System.out.println("Alex: " + phoneBook.getPhonesByName("Alex"));
+        System.out.println("Nikto:" + phoneBook.getPhonesByName(""));
+    }
+
+    private static String[] createArrayWords() {
+        String [] strings = new String[10];
+        strings[0] = "Mama";
+        strings[1] = "Papa";
+        strings[2] = "Puma";
+        strings[3] = "Irbis";
+        strings[4] = "Mama";
+        strings[5] = "Papa";
+        strings[6] = "Kot";
+        strings[7] = "Sobaka";
+        strings[8] = "Dochka";
+        strings[9] = "Sobaka";
+        return strings;
+    }
+
+    private static String[][] createArray() {
+        String[][] array = new String[4][4];
+        for (int i = 0; i < array.length; i++,System.out.println()) {
+            for (int j = 0; j < array[i].length; j++) {
+                array[i][j] = String.valueOf((int)(Math.random() * 10));
+                System.out.print(array[i][j] + " ");
+            }
+        }
+        return array;
     }
 }
 
