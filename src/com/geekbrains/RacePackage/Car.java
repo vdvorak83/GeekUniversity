cb4bea47dd1ea8a80a45ab3ece6ae6155887886c
Шумилин Александр Владимирович <a.v.shumilin@voentelecom.ru>
2017-12-07 17:15:07 +0300

    ready race

--- src/com/geekbrains/RacePackage/Car.java
+++ src/com/geekbrains/RacePackage/Car.java
@@ -1,20 +1,28 @@
 package com.geekbrains.RacePackage;
 
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Car implements Runnable{
     private static int CARS_COUNT;
+    private static AtomicInteger ai;
     static {
+        ai = new AtomicInteger(0);
         CARS_COUNT = 0;
     }
     private Race race;
     private int speed;
     private String name;
+    private CyclicBarrier barrier;
+
     public String getName() {
         return name;
     }
     public int getSpeed() {
         return speed;
     }
-    public Car(Race race, int speed) {
+    public Car(Race race, int speed, CyclicBarrier barrier) {
+        this.barrier = barrier;
         this.race = race;
         this.speed = speed;
         CARS_COUNT++;
@@ -26,11 +34,17 @@ public class Car implements Runnable{
             System.out.println(this.name + " готовится");
             Thread.sleep(500 + (int)(Math.random() * 800));
             System.out.println(this.name + " готов");
+            barrier.await();
+            barrier.await();
+            for (int i = 0; i < race.getStages().size(); i++) {
+                race.getStages().get(i).go(this);
+            }
+            if (ai.incrementAndGet() == 1){
+                System.out.println("Winner - " + this.name);
+            }
+            barrier.await();
         } catch (Exception e) {
             e.printStackTrace();
-        }
-        for (int i = 0; i < race.getStages().size(); i++) {
-            race.getStages().get(i).go(this);
         }
     }
 }
--- src/com/geekbrains/RacePackage/MainRaceClass.java
+++ src/com/geekbrains/RacePackage/MainRaceClass.java
@@ -1,18 +1,25 @@
 package com.geekbrains.RacePackage;
 
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+
 public class MainRaceClass {
     public static final int CARS_COUNT = 4;
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws BrokenBarrierException, InterruptedException {
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
+        CyclicBarrier barrier = new CyclicBarrier(CARS_COUNT + 1);
         Race race = new Race(new Road(60), new Tunnel(), new Road(40));
         Car[] cars = new Car[CARS_COUNT];
         for (int i = 0; i < cars.length; i++) {
-            cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
-        }
-        for (int i = 0; i < cars.length; i++) {
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 10),barrier);
             new Thread(cars[i]).start();
         }
+        barrier.await();
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+        barrier.await();
+        barrier.await();
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
     }
 }
