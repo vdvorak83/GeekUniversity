b4c70a33342cfd54b6b9421edcdb695a26a362e2
Шумилин Александр Владимирович <a.v.shumilin@voentelecom.ru>
2017-12-11 17:14:25 +0300

    home test annotation

--- src/com/geekbrains/Reflection/MainReflectionClass.java
+++ src/com/geekbrains/Reflection/MainReflectionClass.java
@@ -1,16 +1,40 @@
 package com.geekbrains.Reflection;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 public class MainReflectionClass {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
          start(TestingClass.class);
     }
 
-    public static void start(Class c){
+    public static void start(Class c) throws Exception {
+        List<Method> listMethods = new ArrayList<Method>();
         Method[] methods = c.getDeclaredMethods();
         for (Method m : methods) {
-            System.out.println(m);
+            if (m.isAnnotationPresent(Test.class)){
+                if (m.getAnnotation(Test.class).priority() < 1 || m.getAnnotation(Test.class).priority() > 10) throw new RuntimeException("Invaild priority value");
+                listMethods.add(m);
+            }
+        }
+        listMethods.sort((o1, o2) -> o2.getAnnotation(Test.class).priority() - o1.getAnnotation(Test.class).priority());
+
+        for (Method m:methods){
+            if (m.isAnnotationPresent(BeforeSuite.class)){
+                if (listMethods.get(0).isAnnotationPresent(BeforeSuite.class)) throw new RuntimeException("Too many @BeforeSuite");
+                listMethods.add(0,m);
+            }
+            if (m.isAnnotationPresent(AfterSuite.class)){
+                if (listMethods.get(listMethods.size()-1).isAnnotationPresent(AfterSuite.class)) throw new RuntimeException("Too many @AfterSuite");
+                listMethods.add(m);
+            }
+        }
+
+        for (Method m : listMethods) {
+            m.invoke(c.newInstance());
         }
     }
 }
--- src/com/geekbrains/Reflection/TestingClass.java
+++ src/com/geekbrains/Reflection/TestingClass.java
@@ -4,28 +4,45 @@ public class TestingClass {
 
     @BeforeSuite
     public void beforeSuiteMethod(){
-
+        printTestMessage("Bufore suite test");
     }
 
     @AfterSuite
     public void afterSuite(){
-
+        printTestMessage("After suite test");
     }
 
     @Test(priority = 1)
     public void test1(){
-
+        printTestMessage("Test 1");
     }
     @Test(priority = 2)
     public void test2(){
-
+        printTestMessage("Test 2");
     }
-    @Test(priority = 2)
+    @Test(priority = 7)
     public void test3(){
+        printTestMessage("Test 3");
+    }
+    @Test(priority = 5)
+    public void test4(){
+        printTestMessage("Test 4");
+    }
 
+    @Test(priority = 4)
+    public void test5(){
+        printTestMessage("Test 5");
     }
     @Test(priority = 3)
-    public void test4(){
+    public void test6(){
+        printTestMessage("Test 6");
+    }
+    @Test(priority = 9)
+    public void test7(){
+        printTestMessage("Test 7");
+    }
 
+    public void printTestMessage(String textMessage){
+        System.out.println(textMessage);
     }
 }
